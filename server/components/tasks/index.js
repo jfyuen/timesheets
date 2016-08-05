deec15c160ac0c1b4c6defa2df94941b355f3699
Jean-François YUEN <jfyuen@gmail.com>
2016-08-06 00:19:51 +0200

    Fix task test values

--- server/components/tasks/index.js
+++ server/components/tasks/index.js
@@ -8,31 +8,12 @@ router.route('/')
     .post()
     .get(function (req, res) {
         res.status(200).json({
-            '2016-07-04': [
-                {
-                    id: 0,
-                    activity: {id: 0, name: 'Activity 1'},
-                    project: {id: 0, name: 'Project A'},
-                    allocation: {id: 0, name: '1', value: 1},
-                    date: '2016-07-04'
-                }
-            ],
-            '2016-07-05': [
-                {
-                    id: 0,
-                    activity: {id: 0, name: 'Activity 1'},
-                    project: {id: 0, name: 'Project A'},
-                    allocation: {id: 0, name: '1', value: 1},
-                    date: '2016-07-05'
-                }, {
-                    id: 0,
-                    activity: {id: 0, name: 'Activity 2'},
-                    project: {id: 0, name: 'Project b'},
-                    allocation: {id: 2, name: '1/2', value: 0.5},
-                    date: '2016-07-05'
-                }
-            ]
-        })
+            '2016-07-25': [{ id: 0, activity: { id: 0, name: 'Activity 1' }, project: { id: 0, name: 'Project A' }, allocation: { id: 0, name: '1', value: 1 }, date: '2016-07-25' }],
+            '2016-07-28': [
+                { id: 1, activity: { id: 0, name: 'Activity 1' }, project: { id: 0, name: 'Project A' }, allocation: { id: 0, name: '1', value: 1 }, date: '2016-07-28' },
+                { id: 2, activity: { id: 0, name: 'Activity 2' }, project: { id: 0, name: 'Project b' }, allocation: { id: 2, name: '1/2', value: 0.5 }, date: '2016-07-28' }
+                ]
+        });
     });
 
 router.route('/:id')
