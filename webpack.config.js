af5afb3b9c424a4962f98fd5874f524f395c6477
Jean-François YUEN <jfyuen@gmail.com>
2019-02-19 18:03:29 +0100

    deps: update style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
                     presets: ['@babel/preset-env', '@babel/preset-react']
                 }
             },
-             { test: /\.css$/, loader: "style-loader!css-loader" }
+            { test: /\.css$/, loader: "style-loader!css-loader" }
         ]
     },
     externals: {
